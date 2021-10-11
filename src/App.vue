959671622c6264e722796263dfa7de49029ad415
Aleksandra <astarta1107@gmail.com>
2021-10-11 13:25:39 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
-</template>
+
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Open+Sans&display=swap");
+
+*{
+  background-color: #F6F3F3;
+  box-sizing: border-box;
+  color: #343030;
+  padding: 0;
+  margin: 0;
+  font-family: Merriweather, Helvetica, Arial, serif;
+  font-weight: 400;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.wrapp {
+  max-width: 1200px;
+  padding-top: 45px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/store/products.js
@@ -0,0 +1,8 @@
+export default {
+  state: {
+    products: [],
+  },
+  mutations: {},
+  actions: {},
+  getters: {},
+};
